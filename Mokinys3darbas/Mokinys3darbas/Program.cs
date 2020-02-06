4fd3638389fa98b77f88f6cbeb1f77032c483ba6
Weazzzzzzey <heronightmares@gmail.com>
2020-02-06 23:36:12 +0200

    prideta 3 programa

---
+++ Mokinys3darbas/Mokinys3darbas/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MokinysLibrary;
+
+namespace Mokinys3darbas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Mokinys D11 = new Mokinys(11,"Dominykas");
+            D11.Pazymiai.Add(10);
+            D11.Pazymiai.Add(8);
+            D11.Pazymiai.Add(8);
+            D11.Pazymiai.Add(9);
+            D11.Pazymiai.Add(9);
+
+            foreach (var item in D11.Pazymiai)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine(D11.getVidurkis());
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Mokinys3darbas/MokinysLibrary/Mokinys.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MokinysLibrary
+{
+    public class Mokinys
+    {
+        public int ID { get; private set; }
+        public string Vardas { get; private set; }
+        public List<int> Pazymiai { get; set; }
+
+        public Mokinys(int id, string vardas)
+        {
+            this.ID = id;
+            this.Vardas = vardas;
+            this.Pazymiai = new List<int>();
+        }
+
+        public decimal getVidurkis()
+        {
+            decimal sum = 0;
+
+            foreach (var item in Pazymiai)
+            {
+                sum = sum + item;
+            }
+            decimal vidurkis = sum / Pazymiai.Count;
+
+            return vidurkis;
+        }
+
+        
+    }
+}
